b81a67d5006d12cc084f0c25966621e7284bb78f
7Snails <collinb1129@gmail.com>
2017-07-07 13:37:21 -0400

    Update bn.user.js

--- bn.user.js
+++ bn.user.js
@@ -8,9 +8,38 @@
 // @match        https://scratch.mit.edu/*
 // @icon         http://tampermonkey.net/favicon.ico
 // @grant        none
-// @run-at       document-start
+// @run-at       document-end
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js
-// @require      http://coffeescript.org/extras/coffee-script.js
 // ==/UserScript==
 
-console.log("Hello Test!");
+// Create the .remove() function
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+};
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+};
+
+// Remove the Tips button
+document.getElementsByClassName("tips")[0].remove();
+
+// Get Navigation Bar
+var list = document.getElementsByTagName("ul")[0];
+
+// Create the Discuss button
+var discussShell = document.createElement("li");
+discussShell.className = 'link';
+var discussA = document.createElement("a");
+discussA.setAttribute('href', "/discuss");
+var discussSpan = document.createElement("span");
+var discussSpanText = document.createTextNode("Discuss");
+discussSpan.appendChild(discussSpanText);
+discussA.appendChild(discussSpan);
+discussShell.appendChild(discussA);
+
+// Insert the Discuss button and Wiki button
+list.insertBefore(discussShell, list.childNodes[3]);

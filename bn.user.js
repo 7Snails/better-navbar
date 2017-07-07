1d7427395fcbb257a9751ff4ecabe5226f5b4adf
7Snails <collinb1129@gmail.com>
2017-07-07 13:05:59 -0400

    Create bn.user.js

---
+++ bn.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Better Navbar for Scratch
+// @namespace    http://scratch.mit.edu/users/7Snails/
+// @version      0.0.1
+// @description  Makes the Scratch Navigation bar mroe user friendly
+// @author       @7Snails
+// @match        http://scratch.mit.edu/*
+// @match        https://scratch.mit.edu/*
+// @icon         http://tampermonkey.net/favicon.ico
+// @grant        none
+// @run-at       document-start
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js
+// @require      http://coffeescript.org/extras/coffee-script.js
+// ==/UserScript==
+
+print("Hello Test!");
